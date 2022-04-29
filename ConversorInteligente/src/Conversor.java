96933d30b0705301f97a1d3cc1d0cf3db6c24a47
Maicon Antônio Cunico <maiconcunico@gmail.com>
2022-04-29 15:53:48 -0300

    Adicionei o começo do conversor de massa

--- ConversorInteligente/src/Conversor.java
+++ ConversorInteligente/src/Conversor.java
@@ -1,9 +1,10 @@
 public class Conversor extends javax.swing.JFrame {
     
     int operacao;
-    float resultado, curRealDolar=4.5f, curRealEuro=5, curRealLibra=6;
+    float curRealDolar=4.5f, curRealEuro=5, curRealLibra=6;
     float curDolarEuro=1.08f, curDolarLibra=1.30f;
     float curEuroLibra=1.20f;
+    float resultado;
 
     public Conversor() {
         initComponents();
@@ -73,7 +74,28 @@ public class Conversor extends javax.swing.JFrame {
             }
         }
             
+    }
 
+     public void tratarConversãoMassa(){
+        if (cmbInicialM.getSelectedItem().equals("Toneladas")){
+            if (cmbFinalM.getSelectedItem().equals("Toneladas")){
+                resultado = Float.parseFloat(txtValorM.getText());
+            } else if (cmbFinalM.getSelectedItem().equals("Quilogramas (kg)")){
+                resultado = Float.parseFloat(txtValorM.getText()) * 1000f;
+            } else if (cmbFinalM.getSelectedItem().equals("Hectogramas (hg)")){
+                resultado = Float.parseFloat(txtValorM.getText()) * 10000f;
+            } else if (cmbFinalM.getSelectedItem().equals("Decagramas (dag)")){
+                resultado = Float.parseFloat(txtValorM.getText()) * 100000f;
+            } else if (cmbFinalM.getSelectedItem().equals("Gramas (g)")){
+                resultado = Float.parseFloat(txtValorM.getText()) * 1000000f;   
+            } else if (cmbFinalM.getSelectedItem().equals("Decigramas (dg)")){
+                resultado = Float.parseFloat(txtValorM.getText()) * 10000000f;
+            } else if (cmbFinalM.getSelectedItem().equals("Centigramas (cg)")){
+                resultado = Float.parseFloat(txtValorM.getText()) * 100000000f;
+            } else if (cmbFinalM.getSelectedItem().equals("Miligramas (mg)")){
+                resultado = Float.parseFloat(txtValorM.getText()) * 1000000000f;
+            }    
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -92,6 +114,21 @@ public class Conversor extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         btnConverter = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanel3 = new javax.swing.JPanel();
+        jPanel7 = new javax.swing.JPanel();
+        cmbFinalM = new javax.swing.JComboBox<>();
+        cmbInicialM = new javax.swing.JComboBox<>();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        txtResultadoM = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
+        txtValorM = new javax.swing.JTextField();
+        jLabel9 = new javax.swing.JLabel();
+        btnConverterM = new javax.swing.JButton();
+        jPanel4 = new javax.swing.JPanel();
+        jPanel5 = new javax.swing.JPanel();
+        jPanel6 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -167,22 +204,161 @@ public class Conversor extends javax.swing.JFrame {
                         .addComponent(jLabel5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(txtResultado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 65, Short.MAX_VALUE)
                 .addComponent(btnConverter)
                 .addGap(27, 27, 27))
         );
 
         jTabbedPane2.addTab("Moeda", jPanel1);
 
+        cmbFinalM.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Toneladas", "Quilogramas (kg)", "Hectogramas (hg)", "Decagramas (dag)", "Gramas (g)", "Decigramas (dg)", "Centigramas (cg)", "Miligramas (mg)" }));
+
+        cmbInicialM.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Toneladas", "Quilogramas (kg)", "Hectogramas (hg)", "Decagramas (dag)", "Gramas (g)", "Decigramas (dg)", "Centigramas (cg)", "Miligramas (mg)" }));
+
+        jLabel6.setText("Converter de");
+
+        jLabel7.setText("Para");
+
+        txtResultadoM.setEditable(false);
+
+        jLabel8.setText("Valor: ");
+
+        jLabel9.setText("Resultado da Conversão ");
+
+        btnConverterM.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        btnConverterM.setText("Converter");
+        btnConverterM.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnConverterMActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel7Layout = new javax.swing.GroupLayout(jPanel7);
+        jPanel7.setLayout(jPanel7Layout);
+        jPanel7Layout.setHorizontalGroup(
+            jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel7Layout.createSequentialGroup()
+                .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel7Layout.createSequentialGroup()
+                        .addGap(73, 73, 73)
+                        .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(cmbInicialM, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel6))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel7)
+                            .addComponent(cmbFinalM, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel7Layout.createSequentialGroup()
+                        .addGap(44, 44, 44)
+                        .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtValorM, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel8))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtResultadoM, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel9)))
+                    .addGroup(jPanel7Layout.createSequentialGroup()
+                        .addGap(158, 158, 158)
+                        .addComponent(btnConverterM)))
+                .addGap(44, 44, 44))
+        );
+        jPanel7Layout.setVerticalGroup(
+            jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel7Layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel7))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cmbInicialM, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cmbFinalM, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(28, 28, 28)
+                .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel7Layout.createSequentialGroup()
+                        .addComponent(jLabel8)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtValorM, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel7Layout.createSequentialGroup()
+                        .addComponent(jLabel9)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtResultadoM, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+                .addComponent(btnConverterM)
+                .addGap(27, 27, 27))
+        );
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 406, Short.MAX_VALUE)
+            .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel3Layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel7, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 220, Short.MAX_VALUE)
+            .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel3Layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel7, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
+
+        jTabbedPane1.addTab("Massa", jPanel3);
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 406, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 220, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("tab2", jPanel4);
+
+        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
+        jPanel5.setLayout(jPanel5Layout);
+        jPanel5Layout.setHorizontalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 406, Short.MAX_VALUE)
+        );
+        jPanel5Layout.setVerticalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 220, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("tab3", jPanel5);
+
+        javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
+        jPanel6.setLayout(jPanel6Layout);
+        jPanel6Layout.setHorizontalGroup(
+            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 406, Short.MAX_VALUE)
+        );
+        jPanel6Layout.setVerticalGroup(
+            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 220, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("tab4", jPanel6);
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 406, Short.MAX_VALUE)
+            .addComponent(jTabbedPane1)
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 220, Short.MAX_VALUE)
+            .addComponent(jTabbedPane1)
         );
 
         jTabbedPane2.addTab("Unidades de Medida", jPanel2);
@@ -205,6 +381,11 @@ public class Conversor extends javax.swing.JFrame {
         tratarConversãoMoeda();
         txtResultado.setText(Float.toString(resultado));
     }//GEN-LAST:event_btnConverterActionPerformed
+
+    private void btnConverterMActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConverterMActionPerformed
+        tratarConversãoMassa();
+        txtResultadoM.setText(Float.toString(resultado));
+    }//GEN-LAST:event_btnConverterMActionPerformed
 
     /**
      * @param args the command line arguments
@@ -243,16 +424,31 @@ public class Conversor extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnConverter;
+    private javax.swing.JButton btnConverterM;
     private javax.swing.JComboBox<String> cmbFinal;
+    private javax.swing.JComboBox<String> cmbFinalM;
     private javax.swing.JComboBox<String> cmbInicial;
+    private javax.swing.JComboBox<String> cmbInicialM;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
+    private javax.swing.JPanel jPanel6;
+    private javax.swing.JPanel jPanel7;
+    private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTabbedPane jTabbedPane2;
     private javax.swing.JTextField txtResultado;
+    private javax.swing.JTextField txtResultadoM;
     private javax.swing.JTextField txtValor;
+    private javax.swing.JTextField txtValorM;
     // End of variables declaration//GEN-END:variables
 }
